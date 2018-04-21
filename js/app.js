31d50fdc0072921173be37128bd8a5865b22ef18
Spam Konto <spam.konto@onet.pl>
2018-04-21 22:23:31 +0200

    react way?

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 class Key extends React.Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			isPressed: true
+		};
+
+		// this.keyPressed = this.keyPressed.bind(this);
+		this.playSound = this.playSound.bind(this);
+		this.audio = React.createRef();
 	}
+
+  	playSound(evt) {
+  		// console.log(evt.target);
+  		this.audio.current.currentTime = 0;
+  		this.audio.current.play();
+  	}
+
 
 	render(){
 		return (
-			<div className="key" data-key={this.props.dataKey}>
+			<div className="key" data-key={this.props.dataKey} onClick={this.playSound} >
 				<kbd>{String.fromCharCode(this.props.dataKey)}</kbd>
-				<audio data-key={this.props.dataKey} src={"ast/" + this.props.dataKey + ".wav"}></audio>
+				<audio ref={this.audio} data-key={this.props.dataKey} src={"ast/" + this.props.dataKey + ".wav"}></audio>
 			</div>
 		);
 	}
@@ -15,7 +29,6 @@ class Key extends React.Component {
 
 const keyCodesArr = [65,83,68,70,71,72,74,75,76];
 const keyboardArr = keyCodesArr.map((keyCode,i) => <Key key={i} dataKey={keyCode} />);
-
 
 ReactDOM.render(<div className="keys">
                 	{keyboardArr}
