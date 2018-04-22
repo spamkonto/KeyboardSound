1bb47a7a0d645285986dc1cc1fa8a7d80c00473a
Spam Konto <spam.konto@onet.pl>
2018-04-22 18:55:40 +0200

    key events added

--- js/app.js
+++ js/app.js
@@ -5,27 +5,38 @@ class Key extends React.Component {
 			isPressed: true
 		};
 
-		// this.keyPressed = this.keyPressed.bind(this);
 		this.playSound = this.playSound.bind(this);
+		this.removeTransistion = this.removeTransistion.bind(this);
 		this.audio = React.createRef();
 	}
 
-  	playSound(evt) {
-  		// console.log(evt.target);
-  		this.audio.current.currentTime = 0;
-  		this.audio.current.play();
-  	}
+	removeTransistion(evt) {
+		if(evt.propertyName !== 'transform') return;
+		evt.target.classList.remove('playing');
+	}
 
+	playSound(evt) {
+		if(parseInt(this.audio.current.dataset["key"]) === evt.keyCode){
+			this.audio.current.currentTime = 0;
+			this.audio.current.play();
+
+		}
+	}
+
+	componentDidMount() {
+		window.addEventListener("keydown", this.playSound);
+	}
 
 	render(){
 		return (
-			<div className="key" data-key={this.props.dataKey} onClick={this.playSound} >
+			<div className="key" data-key={this.props.dataKey} onTransitionEnd={this.removeTransistion} >
 				<kbd>{String.fromCharCode(this.props.dataKey)}</kbd>
 				<audio ref={this.audio} data-key={this.props.dataKey} src={"ast/" + this.props.dataKey + ".wav"}></audio>
 			</div>
 		);
 	}
 }
+
 
 const keyCodesArr = [65,83,68,70,71,72,74,75,76];
 const keyboardArr = keyCodesArr.map((keyCode,i) => <Key key={i} dataKey={keyCode} />);
